6004cf7a17c6c176f56bc297a52c2e00a377db3f
johns <jsarris11@gmail.com>
2023-12-10 21:09:41 +0200

    add single.php & page.php files to power posts and pages

--- wp-content/themes/customTheme/index.php
+++ wp-content/themes/customTheme/index.php
@@ -1,1 +1,13 @@
-new theme
+<h1><?php echo bloginfo("name");?></h1>
+<p><?php echo bloginfo("description");?></p>
+
+
+<?php
+while(have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php
+}
+?>
