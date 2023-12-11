ef8f4b84e538062a2c3f56de9af5cdebf88bb3ae
johns <jsarris11@gmail.com>
2023-12-11 23:59:39 +0200

    make footer menu dynamic

--- wp-content/themes/university-static-master/functions.php
+++ wp-content/themes/university-static-master/functions.php
@@ -22,6 +22,8 @@ add_action("wp_enqueue_scripts","load_my_styles");
 function course_features() {
     add_theme_support("title-tag");
     register_nav_menu("headerMenuLocation", "Header Menu Location");
+    register_nav_menu("footerLocationOne", "Footer Location One");
+    register_nav_menu("footerLocationTwo", "Footer Location Two");
 }
 
 add_action("after_setup_theme", "course_features");
