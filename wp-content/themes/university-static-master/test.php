e64105e20e05ad208c77a0862cc955566fd465f9
johns <jsarris11@gmail.com>
2023-12-11 21:25:31 +0200

    check some more wp functions

--- wp-content/themes/university-static-master/test.php
+++ wp-content/themes/university-static-master/test.php
@@ -5,7 +5,14 @@ function doubleMe($k) {
     die(var_dump($l)); exit();
     return $l;
 }
-
 doubleMe(20);
+
+//the ones with get just return something, so they need echo
+//the ones not, they echo something
+the_title();
+get_the_title();
+
+the_ID();
+get_the_id();
 
 ?>
